233080deb4ab88a760a745e2c65c57ee7027ceb7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-10 21:13:46 -0400

    first attempt

---
+++ examples/react-scripts-cucumber/cypress/component/WebPack.feature
@@ -0,0 +1,7 @@
+# example feature file taken from
+# https://github.com/jcundill/cypress-cucumber-webpack-typescript-example
+Feature: Working with Webpack
+
+  Scenario: it's alive!
+    Given webpack is configured
+    Then this test should work just fine!
--- examples/react-scripts-cucumber/cypress/plugins/index.js
+++ examples/react-scripts-cucumber/cypress/plugins/index.js
@@ -1,8 +1,9 @@
 // load file preprocessor that comes with this plugin
 // https://github.com/bahmutov/cypress-react-unit-test#install
-const preprocessor = require('cypress-react-unit-test/plugins/react-scripts')
 module.exports = (on, config) => {
-  preprocessor(on, config)
+  // from the root of the project (folder with cypress.json file)
+  config.env.webpackFilename = 'webpack.config.js'
+  require('cypress-react-unit-test/plugins/load-webpack')(on, config)
   // IMPORTANT to return the config object
   // with the any changed environment variables
   return config
